3974c5d8de06c1545542b27c22c86bcc62d69041
Cam-ams <camelia.djenid@gmail.com>
2025-03-31 15:42:12 +0200

    Delete tpFibonacci.js

--- tpFibonacci.js
+++
@@ -1,8 +0,0 @@
-function Fibonacci(nbrMax, actuel = 0, suivant=1, compteur=0){
-    if (compteur > nbrMax){
-        return;
-    }
-    console.log(actuel);
-    fibonnacci(nbrMax, suivant,actuel+suivant, compteur+1);
-}
-fibonnacci(10, 3, 5, 3);
